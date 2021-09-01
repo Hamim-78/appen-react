f2b3cf9028c09999663bc6e85d4c2385e80e7be5
Hamim-78 <shariaearhamim@gmail.com>
2021-09-01 21:57:22 +0600

    appen-prac

--- src/component/appen/Video/index.js
+++ src/component/appen/Video/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Container, Row } from 'react-bootstrap'
 import ReactPlayer from 'react-player'
 import { FaPlay } from 'react-icons/fa';
@@ -6,59 +6,113 @@ import { FaPlay } from 'react-icons/fa';
 const Video = () => {
 
 
-   const [play, setPlay] = useState(false)
 
-
-   return (
+    return (
       <section>
-         <div class="video">
-            <div class="container">
-               <div class="row">
-                  <div class="col-md-12 align-items-center">
-                     <div class="awesome-stars">
-                        <div class="awesome-rec1">
+      <div className="video">
+         <div className="container">
+            <div className="row">
+               <div className="col-md-12 align-items-center">
+                 <div classNameass="awesome-stars">
+                      <div classNamess="awesome-rec1">
+                        <img src="images/fea-shape/Shape2.png" alt="">
+         <div className="video">
+            <div className="container">
+               <div className="row">
+                  <div className="col-md-12 align-items-center">
+                     <div className="awesome-stars">
+                        <div className="awesome-rec1">
                            <img src="images/fea-shape/Shape2.png" alt="">
                            </img>
+                      </div>
+                      <div className="awesome-rec2">
+                        <img src="images/video/Rectangle3.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec2">
+                        <div className="awesome-rec2">
                            <img src="images/video/Rectangle3.png" alt="">
                            </img>
+                      </div>
+                      <div className="awesome-rec3">
+                        <img src="images/video/Rectangle4copy2.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec3">
+                        <div className="awesome-rec3">
                            <img src="images/video/Rectangle4copy2.png" alt="">
                            </img>
+                      </div>
+                      <div className="awesome-rec4">
+                        <img src="images/video/Shape1copy3.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec4">
+                        <div className="awesome-rec4">
                            <img src="images/video/Shape1copy3.png" alt="">
                            </img>
+                      </div>
+                      <div className="awesome-rec5">
+                        <img src="images/video/Shape4copy.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec5">
+                        <div className="awesome-rec5">
                            <img src="images/video/Shape4copy.png" alt="">
                            </img>
+                      </div>
+                      <div className="awesome-rec6">
+                        <img src="images/video/Shape3copy3.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec6">
+                        <div className="awesome-rec6">
                            <img src="images/video/Shape3copy3.png" alt="">
                            </img>
+                      </div>
+                      <div className="awesome-rec7">
+                        <img src="images/video/Shape3copy.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec7">
+                        <div className="awesome-rec7">
                            <img src="images/video/Shape3copy.png" alt="">
                            </img>
+                      </div>
+                      <div className="awesome-rec8">
+                        <img src="images/video/Shape3.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec8">
+                        <div className="awesome-rec8">
                            <img src="images/video/Shape3.png" alt="">
                            </img>
+                     </div>
+                      <div className="awesome-rec9">
+                        <img src="images/video/Shape4copy3.png" alt="">
+                        </img>
                         </div>
-                        <div class="awesome-rec9">
+                        <div className="awesome-rec9">
                            <img src="images/video/Shape4copy3.png" alt="">
                            </img>
                         </div>
                      </div>
+                  </div>
+                  <div className="video-main">
+                     <div className="overlay-video">
+                     </div>
+                     <div className="video-main-item">
+                        <h2 className="video-main-item-title">
+                           View Our App Tutorial
+                        </h2>
+                        <p className="video-main-item-pera">
+                           Lorem Ipsum is simply dummy text of the
+                           printing and typesetting industry. LoremIpsum
+                           has
+                        </p>
+                        <div className="video-main-item-icon">
+                        <FaPlay className="video-main-item-icon-design" />
+                          {/* <ReactPlayer url='images/video/video.mp4' /> */}
+                           <a class="play-btn video-main-item-icon-button" title="This is a video" href=""></a>
                      <div class="video-main">
 
                         {play == true ? <div>
-                           <div class="video-main-item-icon">
+                           <div className="video-main-item-icon">
                               <FaPlay onClick={() => setPlay(false)} className="video-main-item-icon-design" />
-                              <a class="play-btn video-main-item-icon-button" title="This is a video" href=""></a>
+                              <a className="play-btn video-main-item-icon-button" title="This is a video" href=""></a>
                            </div>
                            <ReactPlayer
                               playing={play}
@@ -71,20 +125,20 @@ const Video = () => {
                         </div>
                            :
                            <div>
-                              <div class="overlay-video">
+                              <div className="overlay-video">
                               </div>
-                              <div class="video-main-item">
-                                 <h2 class="video-main-item-title">
+                              <div className="video-main-item">
+                                 <h2 classNames="video-main-item-title">
                                     View Our App Tutorial
                                  </h2>
-                                 <p class="video-main-item-pera">
+                                 <p className="video-main-item-pera">
                                     Lorem Ipsum is simply dummy text of the
                                     printing and typesetting industry. LoremIpsum
                                     has
                                  </p>
-                                 <div class="video-main-item-icon">
+                                 <div className="video-main-item-icon">
                                     <FaPlay onClick={() => setPlay(true)} className="video-main-item-icon-design" />
-                                    <a class="play-btn video-main-item-icon-button" title="This is a video" href=""></a>
+                                    <a className="play-btn video-main-item-icon-button" title="This is a video" href=""></a>
                                  </div>
                               </div>
                            </div>
@@ -96,7 +150,8 @@ const Video = () => {
                </div>
             </div>
          </div>
-      </section>
+      </div>
+   </section>
    )
 }
 
